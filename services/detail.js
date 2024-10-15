ff73ce2770992a354591cd976756ff6dfd83def8
Wibumoe <131719695+wibumoe@users.noreply.github.com>
2024-10-15 10:19:19 +0700

    Update detail.js

--- services/detail.js
+++ services/detail.js
@@ -18,7 +18,8 @@ const Detail = async function (id) {
     const $ = cheerio.loadBuffer(Buffer.from(content));
     $(".venser").children().each(function(){
         const item = $(this);
-        const cover = item.find(".fotoanime > img").attr("src");
+        let cover = item.find(".fotoanime > img").attr("src");
+        let endpoint = item.find("$id").attr("href");
         let title = item.find(".fotoanime > .infozin > .infozingle > p > span").eq(0).text().split(":")[1];
         let title_jp = item.find(".fotoanime > .infozin > .infozingle > p > span").eq(1).text().split(":")[1];
         let rating = item.find(".fotoanime > .infozin > .infozingle > p > span").eq(2).text().split(":")[1];
@@ -31,7 +32,7 @@ const Detail = async function (id) {
         let genre = item.find(".fotoanime > .infozin > .infozingle > p > span").eq(9).text().split(":")[1];
         let synopsis = item.find(".fotoanime > .sinopc").text();
         
-        if(synopsis && rating && title && title_jp && rating && producer && status && total_episode && duration && release_date && studio && genre){
+        if(endpoint && cover && synopsis && rating && title && title_jp && rating && producer && status && total_episode && duration && release_date && studio && genre){
             title = title.trimStart();
             title_jp = title.trimStart();
             rating = rating.trimStart();
@@ -42,9 +43,11 @@ const Detail = async function (id) {
             release_date = release_date.trimStart();
             genre = genre.trimStart();
             synopsis = synopsis.trim();
+            cover = cover.trim();
+            endpoint = endpoint.trim();
         }
         if(!synopsis.length <1){
-            detail.push({title:title,title_jp:title_jp,rating:rating,producer:producer,status:status,total_episode:total_episode,duration:duration,release_date:release_date,genre:genre,synopsis:synopsis});
+            detail.push({endpoint:endpoint,cover:cover,title:title,title_jp:title_jp,rating:rating,producer:producer,status:status,total_episode:total_episode,duration:duration,release_date:release_date,genre:genre,synopsis:synopsis});
         }
     });
     const episode = [];
